efc85a4ebc585d136b2d3f71047a78300d25f166
Kyle Kurkela <kkurkela@bu.edu>
2025-07-29 08:50:18 -0400

    bug fix

--- dcm_crawler_xnat.py
+++ dcm_crawler_xnat.py
@@ -17,7 +17,7 @@ import subprocess
 def find_dicom_files(target_directory):
     try:
         result = subprocess.run(
-            ['find', target_directory, '-path', '/data/xnat/archive/qa', '-prune', '-type', 'f', '-iname', '*.dcm'],
+            ['find', target_directory, '-path', '/data/xnat/archive/qa', '-prune', '-o', '-type', 'f', '-iname', '*.dcm', '-print'],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
@@ -130,6 +130,7 @@ if __name__ == "__main__":
             subject_cache = {}
 
             print('Crawling dcm files...\n')
+            print(len(dcm_files), 'files found.\n')
 
             i = 0
 
@@ -157,8 +158,9 @@ if __name__ == "__main__":
                 #     ('5200','9230')
                 #   The last two fields are not always present, so we check for them
                 #   separately.
-                tags = ['SeriesNumber', 'SeriesDescription', 'StudyDate', ('0051','100F'), ('5200','9230')]
+                tags = ['StudyDescription', 'SeriesNumber', 'SeriesDescription', 'StudyDate', ('0051','100F'), ('5200','9230'), '']
                 dcm_header = dcmread(d, stop_before_pixels=True, specific_tags=tags)
+                projectid_dcm = dcm_header.get('StudyDescription')
                 series_num  = dcm_header.get('SeriesNumber')
                 series_desc = dcm_header.get('SeriesDescription')
                 session_date = dcm_header.get('StudyDate')
@@ -171,7 +173,7 @@ if __name__ == "__main__":
                 checkTwo = ('5200','9230') in dcm_header
                 if checkOne:
                     ele = dcm_header.get(('0051','100F'))
-                    data = [projectid, subjectid, sessionid, session_date_str, series_num, series_desc, dcm_filename, 0, '(0051,100F)', str(ele)]
+                    data = [projectid, projectid_dcm, subjectid, sessionid, session_date_str, series_num, series_desc, dcm_filename, 0, '(0051,100F)', str(ele)]
                     datastore.append(data)
                 elif checkTwo:
                     entry = dcm_header.get(('5200','9230'))
@@ -183,10 +185,10 @@ if __name__ == "__main__":
                             fieldEntry = e.get(('0021','11FE'))[0].get(('0021','114F'))
                         except:
                             fieldEntry = e.get(('0021','10FE'))[0].get(('0021','104F'))
-                        data = [projectid, subjectid, sessionid, session_date_str, series_num, series_desc, dcm_filename, c, '(5200,9230)', str(fieldEntry)]
+                        data = [projectid, projectid_dcm, subjectid, sessionid, session_date_str, series_num, series_desc, dcm_filename, c, '(5200,9230)', str(fieldEntry)]
                         datastore.append(data)
                 else:
-                    data = [projectid, subjectid, sessionid, session_date_str, series_num, series_desc, dcm_filename, 0, '', '']
+                    data = [projectid, projectid_dcm, subjectid, sessionid, session_date_str, series_num, series_desc, dcm_filename, 0, '', '']
                     datastore.append(data)
                 
                 if len(datastore) > BATCH_SIZE:
